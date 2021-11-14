d2b75272f739314ba9b13a6d28aad2b48332d56e
neels99 <vasile.danil1@gmail.com>
2021-11-14 15:39:24 +0300

    saved contour in image

--- generator/test_letter_a/test_letter.py
+++ generator/test_letter_a/test_letter.py
@@ -8,13 +8,26 @@ let_a = Image.open("../../images/a2.png", "r")
 im = Image.new('RGB', (15000,15000), (255,255,255))
 draw = ImageDraw.Draw(im)
 _xy = []
-f = open("dots3.txt", "r")
+f = open("dots_inside.txt", "r")
 for l in f.readlines():
     _xy += [tuple(map(float, l.split(' ')[:2]))]
+f.close()
+
+contour = []
+f = open("dots_contour.txt", "r")
+for l in f.readlines():
+    contour += [tuple(map(float, l.split(' ')[:2]))]
+f.close()
+
+print("Parsed contour points: {0}; of which is located in dots_inside.txt: {1}".format(len(contour), len([x for x in contour if x in _xy])))
+
 
 print("Parsed points: {0}".format(len(_xy)))
 for _ in range(len(_xy)-wanna_dots):
-    _xy.pop(random.randint(0, len(_xy)-1))
+    index = random.randint(0, len(_xy)-1)
+    while _xy[index] in contour:
+        index = random.randint(0, len(_xy)-1)
+    _xy.pop(index)
 print("Removed other points, len now: {0}".format(len(_xy)))
 
 # __xy = []
